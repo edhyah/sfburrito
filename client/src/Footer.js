4f5dbe8aa40a71d496c0515ef1df1b5599591ac6
Edward Ahn <edhyah@gmail.com>
2022-04-03 23:47:47 -0700

    Simplified footer with just buttons/icons

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -4,22 +4,19 @@ import { Icon } from '@iconify/react';
 
 function Footer() {
     return (
-        <div className="w-[50%] pt-8 mx-auto text-center">
-            <p>
-                Don't see your favorite tacqueria?<br />
-                Upset that your favorite tacqueria isn't at the top?
-            </p>
-            <div className="flex justify-center">
-                <p className="mr-3">Reach out to me on Twitter!</p>
+        <div className="w-[50%] pt-8 mx-auto text-center flex mb-8 justify-center">
+            <div className="flex bg-[#4267B2] w-fit h-fit px-2 rounded-md cursor-pointer">
+                <FontAwesomeIcon icon={faThumbsUp} style={{color:"#ffffff"}} className="py-1" size="xs" />
+                <p className="text-[#ffffff] pl-1 text-sm">Like 3.2K</p>
+            </div>
+            <div className="mx-3">
                 <a href="https://twitter.com/edwardahn9?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-screen-name="false" data-show-count="false">Follow @edwardahn9</a>
             </div>
-            <div className="flex justify-center pt-5">
-                <p className="mr-3">If you enjoyed this, punch this like button!</p>
-                <FontAwesomeIcon icon={faThumbsUp} style={{color:"blue"}} className="py-1" />
-            </div>
-            <div className="w-4/5 pt-5 mx-auto mb-8">
-                <p>Even better, send me a buck or two to fund my next burrito (and server costs)!</p>
-                <Icon icon="bxl-venmo" color="#3d95ce" />
+            <div className="flex bg-[#3d95ce] h-fit px-2 rounded-md cursor-pointer">
+                <div className="py-1">
+                    <Icon icon="bxl:venmo" color="white" height="12" />
+                </div>
+                <p className="text-[#ffffff] pl-1 text-sm">Buy me a burrito</p>
             </div>
         </div>
     );
