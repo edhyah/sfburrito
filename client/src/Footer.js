32e8f3a8e8f90d90c7af95d870fd6a26e274a8e9
Edward Ahn <edhyah@gmail.com>
2022-04-04 01:28:07 -0700

    Add Venmo interaction

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -8,12 +8,14 @@ export default function Footer() {
             <div className="ml-3">
                 <a href="https://twitter.com/edwardahn9?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-screen-name="false" data-show-count="false" />
             </div>
-            <div className="flex bg-[#3d95ce] h-fit px-2 rounded-md cursor-pointer hover:bg-[#327cad] ml-3">
-                <div className="py-1">
-                    <Icon icon="bxl:venmo" color="white" height="12" />
+            <a href="https://venmo.com/code?user_id=1959207003750400831&created=1649060579.625554&printed=1">
+                <div className="flex bg-[#3d95ce] h-fit px-2 rounded-md cursor-pointer hover:bg-[#327cad] ml-3">
+                    <div className="py-1">
+                        <Icon icon="bxl:venmo" color="white" height="12" />
+                    </div>
+                    <p className="text-[#ffffff] pl-1 text-sm">Buy me a 🌯</p>
                 </div>
-                <p className="text-[#ffffff] pl-1 text-sm">Buy me a 🌯</p>
-            </div>
+            </a>
         </div>
     );
 }
