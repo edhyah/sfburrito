a8f0cc9f089b84858918d3b4908a7f1ee535e4d0
Edward Ahn <edhyah@gmail.com>
2022-04-11 23:50:56 -0700

    Made title bold and added replaced period in subtitle with burrito emoji

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,10 +2,10 @@ export default function Header() {
     return (
         <div className="pt-8 pb-5 text-center">
             <h1 className="text-3xl font-bold">
-                🌯 Who has the best Mission burrito in SF? 🌯
+                Who has the best Mission burrito in SF?
             </h1>
             <p className="pt-2">
-                It's time to end the debate once and for all.
+                It's time to end the debate once and for all 🌯
             </p>
         </div>
     );
